2c86a44ec964604f0ed9e203b7796758700fd253
Gustavo <profissional.gustavoa@gmail.com>
2025-07-21 16:56:26 -0300

    feat: Create User and Role entities with Many-to-Many relationship

---
+++ src/main/java/com/gustavooarantes/foodhub/domain/Role.java
@@ -0,0 +1,30 @@
+package com.gustavooarantes.foodhub.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+
+@Entity
+@Table(name = "roles")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Role implements GrantedAuthority {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name; // ROLE_ADMIN, ROLE_CLIENT
+
+    @Override
+    public String getAuthority() {
+        return this.name;
+    }
+
+}
